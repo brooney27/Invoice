b00259091a5ce10d7ee34fc1a789f0a41e44df29
brooney27 <brooney278@gmail.com>
2016-06-14 15:33:46 -0400

    ayy

---
+++ src/Invoice.java
@@ -0,0 +1,39 @@
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Invoice {
+	public static void main(String[] args){
+		NumberFormat nf= NumberFormat.getInstance();
+		nf.setMaximumFractionDigits(2);
+		nf.setMinimumFractionDigits(2);
+		nf.setRoundingMode(RoundingMode.HALF_UP);
+		
+		Scanner in = new Scanner(System.in);
+		
+		double input;
+		double[] inputs = new double[100];
+		int i = 0;
+		double sum = 0;
+		
+		System.out.println("Enter tax %: ");
+		double tax = in.nextDouble();
+		
+		while(1==1){
+			System.out.println("Enter Item Price: ");
+			input = in.nextDouble();
+			if(input == 0)break;
+			inputs[i] = input;
+			i++;
+			sum += input;
+		}
+		
+		for(int j = 0; j < i; j++){
+			System.out.println("Price #" + (j+1) + " : " + nf.format(inputs[j]));
+		}
+		double taxTotal = sum*tax;
+		System.out.println("Subtotal: " + nf.format(sum));
+		System.out.println("Tax: " + nf.format(taxTotal));
+		System.out.println("Grand Total: " + nf.format(sum+taxTotal));
+	}
+}
